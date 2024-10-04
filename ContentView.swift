acbc5b016902623b9004215ca3bc30362009e7c5
GpaDoctor <149095410+GpaDoctor@users.noreply.github.com>
2024-10-04 15:37:18 +0800

    Update ContentView.swift

---
+++ ContentView.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        NavigationStack {
+           NavigationLink( destination:
+             TimetableView(){
+              Image("timetable")
+                 .resizable(resizingMode: .stretch)
+                 .aspect
+        
+              
